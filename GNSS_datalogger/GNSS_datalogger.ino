c10503b1502f8f077549196919b19273bf511ee5
averywallis <averybwallis@gmail.com>
2019-11-02 12:45:35 -0400

    Added GNSS Data logger
    Logs outgoing GPS NMEA strings

--- GNSS_datalogger/GNSS_datalogger.ino
+++ GNSS_datalogger/GNSS_datalogger.ino
@@ -1,6 +1,8 @@
+#include <SPI.h>
+#include <SD.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2, 3); // RX, TX
+SoftwareSerial mySerial(2, 3); //RX, TX
 
 char message[256];
 char message_length = 255;
@@ -9,23 +11,34 @@ char new_byte = 0;
 int index = 0;
 bool message_done = false;
 
-bool USB_serial = true;
+bool USB_serial = false;
+const int chipSelect = 4;
 
 void setup() {
   // put your setup code here, to run once:
-  //  delay(2000); //safe boot
   memset(message, '0', 256);
   if (USB_serial) {
     Serial.begin(115200);
     Serial.println("Begin USB Serial");
   }
-  else{
+  if (USB_serial) {
+    Serial.print("Initializing SD card...");
   }
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    if (USB_serial) {
+      Serial.println("Card failed, or not present");
+    }
+    // don't do anything more:
+    while (1);
+  }
+  else {
+    if (USB_serial) {
+      Serial.println("card initialized.");
+    }
+  }
   mySerial.begin(9600);
-  mySerial.write("test");
 }
 
 void loop() {
@@ -58,7 +71,7 @@ void loop() {
       else { //else continue
         message[index] = new_byte;
         index++;
-        if(index > 255){
+        if (index > 255) {
           if (USB_serial) {
             Serial.print("Overflow");
           }
@@ -66,19 +79,25 @@ void loop() {
         }
       }
     }
-    if (message_done) {
-      digitalWrite(13, HIGH);
-      if (USB_serial) {
+    if (message_done) { //if we have a complete message
+      if (USB_serial) { //if we are printing to Serial
         Serial.println("message done");
-        for(int x = 0; x < index; x++){
+        for (int x = 0; x < index; x++) {
           Serial.print(message[x]);
         }
         Serial.println("");
       }
+      //Now log it to the SD card
+      File dataFile = SD.open("GNSS.txt", FILE_WRITE); //open the SD card
+      if (dataFile) { //if the file is available, write to it:
+        dataFile.println((String)message); //write the string version of the message to the file
+        dataFile.close(); //close the file
+      }
+      // if the file isn't open, pop up an error:
+      else {
+        Serial.println("error opening data file");
+      }
       clear_message();
-    }
-    else{
-      digitalWrite(13, LOW);
     }
   }
 }
